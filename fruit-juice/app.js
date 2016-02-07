bc91c6622d534c71dd2e97afc87657c209e473a4
Stuwert <stuart@urback.net>
2016-02-06 23:54:27 -0700

    Working Jar obj

--- fruit-juice/app.js
+++ fruit-juice/app.js
@@ -9,6 +9,7 @@ Jar.prototype.add = function(amount, type){
   }else{
     this[type] = {
       amount : amount,
+
     }
     this.types += 1;
   }
@@ -20,15 +21,33 @@ Jar.prototype.add = function(amount, type){
 
 Jar.prototype.pourOut = function(amount){
   this.total -= amount;
+  this.total = this.total < 0 ? 0 : this.total;
   for (concentrate in this){
-    this[concentrate].amount -= amount /this.types
+    if (this.total === 0){
+      this[concentrate].amount = 0
+      this[concentrate].concentration = 0;
+    } else{
+      this[concentrate].amount -= amount / this.types
+    }
   }
 }
 
+Jar.prototype.getTotalAmount = function(){
+  return this.total;
+}
+
+Jar.prototype.getConcentration = function(type){
+  if(this[type]){
+    return this[type].concentration;
+  }else{
+    return 0;
+  }
+}
 
 var test = new Jar();
 
 test.add(100, "banana")
 test.add(100, "apple")
 test.pourOut(100)
+console.log(test.getConcentration('banana'));
 console.log(test);
