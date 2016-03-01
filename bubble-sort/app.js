7beccb8e5ea04eeb044c33496a432373b3e01410
Stuart Urback <stuart@urback.net>
2016-03-01 08:01:59 -0700

    Refactor

--- bubble-sort/app.js
+++ bubble-sort/app.js
@@ -1,20 +1,17 @@
 module.exports = function (arr){
 
-  var unclean = true;
+  var unclean = arr.length;
 
-  while(unclean){
-    var counter = 0;
+  while(unclean > 0){
     for(var i = 0; i < arr.length - 1; i++){
       if (arr[i] > arr[i+1]){
         var holder = arr[i];
         arr[i] = arr[i+1]
         arr[i+1] = holder;
+        unclean++;
       }else{
-        counter++;
+        unclean--;
       }
-    }
-    if(counter === arr.length - 1){
-      unclean = false;
     }
   }
   return arr
--- bubble-sort/test/test.js
+++ bubble-sort/test/test.js
@@ -12,24 +12,19 @@ var answerthree = [-1, 1, 4, 40, 345]
 var four = [0, 0, 0, 0, 0, 0];
 var answerfour = four;
 
-// describe('Sorts an array from lowest to highest', function(){
-//   it('loops through an array step by step', function(){
-//     expect(bubbleSorter(one).join("")).to.equal(answerone.join(""));
-//   })
-//   it('does more complex stuff', function(){
-//     expect(bubbleSorter(two).join("")).to.equal(answertwo.join(""));
-//   })
-//
-//   it('does even more complex stuff', function(){
-//     expect(bubbleSorter(three).join("")).to.equal(answerthree.join(""));
-//   })
-//
-//   it('can handle an array of 0s', function(){
-//     expect(bubbleSorter(four).join("")).to.equal(answerfour.join(""));
-//   })
-// })
+describe('Sorts an array from lowest to highest', function(){
+  it('loops through an array step by step', function(){
+    expect(bubbleSorter(one).join("")).to.equal(answerone.join(""));
+  })
+  it('does more complex stuff', function(){
+    expect(bubbleSorter(two).join("")).to.equal(answertwo.join(""));
+  })
 
-console.log(bubbleSorter(one));
-console.log(bubbleSorter(two));
-console.log(bubbleSorter(three));
-console.log(bubbleSorter(four));
+  it('does even more complex stuff', function(){
+    expect(bubbleSorter(three).join("")).to.equal(answerthree.join(""));
+  })
+
+  it('can handle an array of 0s', function(){
+    expect(bubbleSorter(four).join("")).to.equal(answerfour.join(""));
+  })
+})
