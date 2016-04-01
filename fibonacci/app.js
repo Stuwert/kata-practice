651ecdab3416ed4c67f8fd40cf37cfd9985dcfcd
Stuart Urback <stuart@urback.net>
2016-04-01 14:33:18 -0600

    Messing around

--- fibonacci/app.js
+++ fibonacci/app.js
@@ -1,41 +1,30 @@
 "use strict"
 
-// var fib = [1,1,2,3,5,8,13,21 ];
-
-const THING = bing();
-
-function bing(){
-  return 5;
-}
-
-// function fibb(incoming, a = 0, b = 1){
-//   let newA = b;
-//   let newB = b + a;
-//   if (incoming === 1){
-//     return newB;
+// function fibb(incoming, a=0, b=1){
+//   if(incoming <= 1){
+//     return b + a;
 //   }else{
-//     return fibb(incoming - 1, newA, newB);
+//     return fibb(incoming - 1, b, b+a);
 //   }
 // }
 
+console.log(fibb(500));
+
 let fibb = (incoming, a = 0, b = 1) => {
-  let newA = b;
-  let newB = b + a;
   if (incoming <= 1){
-    return newB;
+    return b + a;
   }else{
-    return fibb(incoming - 1, newA, newB);
+    return fibb(incoming - 1, b, b + a);
   }
 }
 
-let fibbNoRecur = (index) => {
-  let fibArray = [0, 1];
-  while(index > 0){
-    fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2])
-    index--;
-  }
-  return fibArray.pop();
-}
+// let fibbNoRecur = (index) => {
+//   let fibArray = [0, 1], counter = 0;
+//   while(index > counter){
+//     fibArray.push(fibArray[counter + 1] + fibArray[counter])
+//     counter++;
+//   }
+//   return fibArray.pop();
+// }
 
-console.log(fibbNoRecur(304));
-console.log(fibb(304));
+// console.log(fibbNoRecur(5));
